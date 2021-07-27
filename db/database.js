bc4ee5699e8f8a6df3d3533807f63fe6fd7f6994
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-27 00:25:17 -0700

    viewAllRoles

--- db/database.js
+++ db/database.js
@@ -3,12 +3,20 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password'
+    password: 'password',
+    database: 'employees'
 });
 
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL');
+
 });
+
+
+
+
+
+
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 const inquirer = require('inquirer');
+const db = require('./db/database');
+
+const startDirectory = () => {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a role', 'add an employee', 'update an employee role']
+        })
+        .then(data => {
+            const { menu } = data;
+            switch (menu) {
+                case 'view all departments':
+                    viewAllDepartments();
+                    break;
+                case 'view all roles':
+                    viewAllRoles();
+                    break;
+                case 'view all employees':
+                    viewAllEmployees();
+                    break;
+                case 'add a role':
+                    addRole();
+                    break;
+                case 'add an employee':
+                    addEmployee();
+                    break;
+                case 'update an employee role':
+                    updateEmployeeRole();
+                    break
+            }
+        })
+}
+
+const viewAllDepartments = () => {
+    const sql = 'SELECT * FROM department'
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+        startDirectory();
+    })
+}
+
+const viewAllRoles = () => {
+    const sql = 'SELECT * FROM role'
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+        startDirectory();
+    })
+}
+
+const viewAllEmployees = () => { }
+
+const addRole = () => { }
+
+const addEmployee = () => { }
+
+const updateEmployeeRole = () => { }
+
+
+startDirectory()
